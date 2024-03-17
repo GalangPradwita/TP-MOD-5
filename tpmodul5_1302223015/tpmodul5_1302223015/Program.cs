f431e9ff3981889715cc5cd16dc5058ce1e502c8
braacellettt <104008593+GalangPradwita@users.noreply.github.com>
2024-03-18 02:01:36 +0700

    menambahkan class DataGeneric

--- tpmodul5_1302223015/tpmodul5_1302223015/Program.cs
+++ tpmodul5_1302223015/tpmodul5_1302223015/Program.cs
@@ -2,9 +2,35 @@
 {
     internal class Program
     {
+
+        public class DataGeneric<T>
+        {
+            private T data;
+            public DataGeneric(T data) { 
+                this.data = data;
+            
+            }
+
+            public void PrintData()
+            {
+                Console.WriteLine("Data yang tersimpan adalah " + data);
+            }
+        }
+
+        public class HaloGeneric
+        {
+            public void SapaUser(String username)
+            {
+                Console.WriteLine("Halo User "+ username);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            HaloGeneric namaPanggilan = new HaloGeneric();
+            namaPanggilan.SapaUser("Galang");
+
+            DataGeneric<String> data = new DataGeneric<String>("1302223015");
+            data.PrintData();
         }
     }
 }
--- tpmodul5_1302223015/tpmodul5_1302223015/obj/Debug/net8.0/tpmodul5_1302223015.AssemblyInfo.cs
+++ tpmodul5_1302223015/tpmodul5_1302223015/obj/Debug/net8.0/tpmodul5_1302223015.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul5_1302223015")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+653ba12befd43040e2708e3e256dcaea1dfce244")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul5_1302223015")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul5_1302223015")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
