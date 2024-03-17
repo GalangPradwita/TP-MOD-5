c4d148843317da50bcad1e7df16f5c21da314850
braacellettt <104008593+GalangPradwita@users.noreply.github.com>
2024-03-18 01:08:40 +0700

    menambahkan class HaloGeneric dengan method

--- tpmodul5_1302223015/tpmodul5_1302223015/Program.cs
+++ tpmodul5_1302223015/tpmodul5_1302223015/Program.cs
@@ -2,9 +2,18 @@
 {
     internal class Program
     {
+
+        public class HaloGeneric
+        {
+            public void SapaUser(String username)
+            {
+                Console.WriteLine("Halo User" +" " + username);
+            }
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            HaloGeneric namaPanggilan = new HaloGeneric();
+            namaPanggilan.SapaUser("Galang");
         }
     }
 }
